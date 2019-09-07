f6cb89d9b36fad97b48e93596b59877e0c7a4eff
Musadiq Khan <Musadiq.Khan@gmail.com>
2019-09-07 23:51:27 +0500

    Updates

---
+++ precache-manifest.01ff4bfe450ab13beb663b3a8dc4197d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9528c333224cfa522fe0f93ee877988",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "944ab401132db1c9aefb",
+    "url": "/facerecognition/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "15b23bb0d37d366bd4bd",
+    "url": "/facerecognition/static/css/main.db5b800f.chunk.css"
+  },
+  {
+    "revision": "944ab401132db1c9aefb",
+    "url": "/facerecognition/static/js/2.d93dd904.chunk.js"
+  },
+  {
+    "revision": "15b23bb0d37d366bd4bd",
+    "url": "/facerecognition/static/js/main.dd2081e8.chunk.js"
+  },
+  {
+    "revision": "51ed884222cb89a90872",
+    "url": "/facerecognition/static/js/runtime~main.cb43e17a.js"
+  }
+]);
